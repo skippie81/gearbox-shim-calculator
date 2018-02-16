f0fa5775449425793f682f778cf538ece011e85a
Joeri Scapicchio <31068429+skippie81@users.noreply.github.com>
2018-02-16 19:01:23 +0100

    some refactor and change logic to start with largest set and check wat set is largest that could possibly hold results, do not calculate any larger sets

--- cmd/calculate.go
+++ cmd/calculate.go
@@ -21,16 +21,20 @@ var (
 	targetThickness = flag.Int("t",176,"Target thickness")
 	targetMargin = flag.Int("m",1,"Margin on target")
 	shimList = flag.String("shimlist","24,27,30,33,36,39,42,45,69,93,111,117,141","Comma seperated list of shims")
-	maxIterations = flag.Int("M",6,"Maximums iteration depht (max shims in one set)")
-	threads = flag.Int("threads",2,"Threads to use")
-
-	dataSet = &DataSet{make(map[int]*ResultList)}
-	runningThreads *int
+	maxIterations = flag.Int("M",10,"Maximums iteration depht (max shims in one set)")
+	threads = flag.Int("threads",2,"Threads to use") // not used for now
 )
+
+
+// this holds the list of avialable shims
+// loaded on startup
 
 type ShimList struct {
 	Shims		[]int        `json:"shims"`
 }
+
+// functions to create the shimlist from input string
+// comma separated set op integers
 
 func newShimList(s []int) (sl ShimList){
 	sort.Sort(sort.Reverse(sort.IntSlice(s)))
@@ -53,16 +57,27 @@ func newShimListFromString(s string)(sl ShimList, err error){
 	return
 }
 
+// A resulstet is ONE list of shims used
+// Generating a specific thikness
 type ResultSet struct {
 	Thickness int        `json:"thikness"`
 	Shims     []int      `json:"shims"`
 }
 
+// A resultlist holds a list of resultsets
+// could be all possible combinations or just a subset that matches the thickness criteria
 type ResultList struct {
 	Results      []ResultSet     `json:"results"`
-	ResultLength int             `json:"result_length"`
 }
 
+// a dataset holds a map with pointers to ResultList
+// is used to point to a specific resultlist of a specific lenght
+type DataSet struct {
+	Data 	map[int]*ResultList
+}
+
+
+// Implementing the String Interfac for ResultList and Result set for outpot
 func (rs ResultSet) String() string{
 	return fmt.Sprintf("%v  -> %v",rs.Thickness,rs.Shims)
 }
@@ -89,9 +104,8 @@ func (rl ResultList) String() (str string){
 	return
 }
 
-type DataSet struct {
-	Data 	map[int]*ResultList
-}
+
+// generation of the resultsets
 
 func NewSets(setLengt,shims, iteration int) int {
 	if setLengt == 1 {
@@ -121,13 +135,7 @@ func StartIndex(lenght, setLenght, shims, iteration int) int {
 	return lenght - NewSets(setLenght,shims, iteration)
 }
 
-func GenArrays(l int,sl ShimList) (lists []ResultSet){
-
-	if dataSet.Data[l] != nil {
-		fmt.Printf("Cache hit for %v\n",l)
-		return dataSet.Data[l].Results
-	}
-
+func GenArrays(l int,sl ShimList) (rl ResultList){
 	if l == 1 {
 		for _,shim := range sl.Shims {
 			t := []int{shim}
@@ -135,97 +143,57 @@ func GenArrays(l int,sl ShimList) (lists []ResultSet){
 				Thickness: 	shim,
 				Shims:		t,
 			}
-			lists = append(lists,rs)
+			rl.Results = append(rl.Results,rs)
 		}
 	} else {
-		sublists := GenArrays( l - 1 , sl )
-
+		sublist := GenArrays( l - 1 , sl )
 		for c,shim := range sl.Shims {
-			for i := StartIndex(len(sublists),l,len(sl.Shims),c); i < len(sublists); i++ {
+			for i := StartIndex(len(sublist.Results),l,len(sl.Shims),c); i < len(sublist.Results); i++ {
 				t := make([]int,l-1)
-				copy(t,sublists[i].Shims)
+				copy(t,sublist.Results[i].Shims)
 				t = append(t,shim)
 
 				rsn := ResultSet{
-					Thickness:	sublists[i].Thickness + shim,
+					Thickness:	sublist.Results[i].Thickness + shim,
 					Shims:		t,
 				}
-				lists = append(lists,rsn)
+				rl.Results = append(rl.Results,rsn)
 			}
 		}
 	}
 	return
 }
 
-func (r *ResultList) Generate(l int,shims ShimList) {
-	r.Results = GenArrays(l,shims)
-	r.ResultLength = l
-}
 
+// check valid results
 func (oklist *ResultList) LoadResults(target,tolerance int,rl *ResultList){
-	stop := true
 	for _,rs := range rl.Results {
 		if rs.Thickness <= target + tolerance {
-			stop = false
 			if math.Abs(float64(rs.Thickness - target)) <= float64(tolerance) {
 				oklist.Results = append(oklist.Results, rs)
 			}
 		}
 	}
-
-	if stop {
-		// if all sets in current list are lager than target + tolerance
-		// we just set maxitterations to 1 so there are no new calculations starting
-		// as longer sets will never return smaller thikness than thinnest combination of this current set
-		fmt.Printf("Set of %v lenght returned no combinations smaller than %v, stopping further calculations",rl.ResultLength,target+tolerance)
-		*maxIterations = 1
-	}
 }
 
-func RunCalculationThread(lenght, target, toleration int,shims ShimList,foundList *ResultList,c chan *ResultList){
-	rl := ResultList{}
-	rl.Generate(lenght,shims)
 
-	c <- &rl
+func Calculate(target, tolerance, maxsetlenght int, shims ShimList) (oklist ResultList){
+	d := DataSet{make(map[int]*ResultList)}
 
-	foundList.LoadResults(target,toleration,&rl)
-	fmt.Printf("Stop thread for setlenght %v\n",lenght)
-	*runningThreads--
-}
-
-func CacheBuilder(c chan *ResultList){
-	for rl := range c {
-		if dataSet.Data[rl.ResultLength] == nil {
-			fmt.Printf("save setlenght %v to cache\n",rl.ResultLength)
-			dataSet.Data[rl.ResultLength] = rl
-		}
-	}
-}
-
-func Calculate(target, tolerance int, shims ShimList) (oklist ResultList){
-	i :=1
-	t := 0
-	runningThreads = &t
-
-	c := make(chan *ResultList)
-	go CacheBuilder(c)
-
-	for i <= *maxIterations {
-		for *runningThreads < *threads -1 && i < *maxIterations {
-			*runningThreads++
-			fmt.Printf("Starting thread %d for setlenght %v\n",*runningThreads,i)
-			go RunCalculationThread(i,target,tolerance,shims,&oklist,c)
-			i++
-		}
-		*runningThreads++
-		fmt.Printf("Starting thread %d for setlenght %v\n",*runningThreads,i)
-		RunCalculationThread(i,target,tolerance,shims,&oklist,c)
-		i++
+	for i := maxsetlenght; i > 0; i-- {
+		a := GenArrays(i,shims)
+		d.Data[i] = &a
 	}
 
-	close(c)
-
+	for _,rl := range d.Data{
+		oklist.LoadResults(target,tolerance,rl)
+	}
 	return
+}
+
+func (s *ShimList) getLargestPossibleSet(target, tolerance int) int {
+	smallestShim := s.Shims[len(s.Shims)-1]
+	return int(( target + tolerance ) / smallestShim)
 }
 
 func main() {
@@ -240,15 +208,23 @@ func main() {
 	target := *targetThickness
 	tolerance := *targetMargin
 
+	fmt.Printf("Available shims: %v\n",shims.Shims)
+	fmt.Printf("Target: %v  (tolerance: %v)\n",target,tolerance)
+
+	if m := shims.getLargestPossibleSet(target,tolerance); *maxIterations > m {
+		fmt.Printf("A set op %v shims is the largest possible set that could have a thikness within required range.\n",m)
+		fmt.Println("Setting max set of itterations accordingly")
+		*maxIterations = m
+	}
+
 	if *threads >= *maxIterations {
 		fmt.Printf("Setting threads to %v as this is the max set of itterations\n",*maxIterations)
 		*threads = *maxIterations
 	}
 
-	fmt.Printf("Available shims: %v\n",shims)
-	fmt.Printf("Target: %v  (tolerance: %v)\n",target,tolerance)
+
 	fmt.Printf("Using %v threads for calculation\n",*threads)
 
-	result := Calculate(target,tolerance,shims)
+	result := Calculate(target,tolerance,*maxIterations,shims)
 	fmt.Printf("%s",result)
 }
